603fe19010e7cdebb0241f2fc273f8372ec0dcaf
Patrick Shea <patrickshea814@gmail.com>
2019-04-22 23:11:16 -0400

    restarted, new tools, new files.

--- javascript/colors.js
+++ javascript/colors.js
@@ -1,14 +1,15 @@
-//array to create color palette
+// Color Palette Array
 let colors = ['blue', 'red', 'yellow', 'green', 'orange', 'purple', 'black'];
 
-let setSwatch = (e) => {
-    //identify swatch
-    let swatch = e.target;
+
+let setPalette = (event) => {
+    //identify palette
+    let palette = event.target;
     //set color
-    setColor(swatch.style.backgroundColor);
+    setColor(palette.style.backgroundColor);
     //give active class
     //leave space in active to separate classes
-    swatch.className += ' active ';
+    palette.className += ' active ';
 }
 
 
@@ -17,17 +18,17 @@ let setColor = (color) => {
     context.strokeStyle = color;
     let active = document.getElementsByClassName('active')[0];
     if (active) {
-        active.className = 'swatch';
+        active.className = 'palette';
     }
 }
 
 
 for (let i = 0, n = colors.length; i < n; i++) {
-    let swatch = document.createElement('div');
-    swatch.className = 'swatch';
-    swatch.style.backgroundColor = colors[i];
-    swatch.addEventListener('click', setSwatch);
-    document.getElementById('colors').appendChild(swatch);
+    let palette = document.createElement('div');
+    palette.className = 'palette';
+    palette.style.backgroundColor = colors[i];
+    palette.addEventListener('click', setPalette);
+    document.getElementById('colors').appendChild(palette);
 }
 
-setSwatch({ target: document.getElementsByClassName('swatch')[0] });
+setPalette({ target: document.getElementsByClassName('palette')[0] });
