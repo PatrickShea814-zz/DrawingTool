ec64935d8b0c5c95f0c87928693c099ad6c04e2c
Patrick Shea <patrickshea814@gmail.com>
2019-04-24 22:14:29 -0400

    reorganizing files and functions/commands

--- DrawingCLI/index.js
+++ DrawingCLI/index.js
@@ -3,6 +3,12 @@ const clear = require('clear');
 const figlet = require('figlet');
 var inquirer = require("inquirer");
 
+
+var createCanvas = require('./lib/canvas');
+var createLine = require('./lib/line');
+var createRectangle = require('./lib/rectangle');
+var bucketFill = require('./lib/bucket');
+
 clear();
 console.log(
     chalk.yellow(
@@ -12,42 +18,45 @@ console.log('Welcome to my Drawing Tool');
 console.log('------------------');
 console.log('Below are the available drawing tools.');
 console.log('------------------');
+module.exports = {
+    commands: inquirer.prompt([
+        {
+            type: "list",
+            name: "toolCommands",
+            message: "What drawing tool would you like to use?",
+            choices: ["Create Canvas", "Draw Line 1", "Draw Line 2", "Draw Rectangle", "Bucket Fill", "Clear Canvas"]
+        }
+    ]).then(function (command, screenBuffer) {
 
-inquirer.prompt([
-    {
-        type: "list",
-        name: "toolCommands",
-        message: "What drawing tool would you like to use?",
-        choices: ["Create Canvas", "Draw Line 1", "Draw Line 2", "Draw Rectangle", "Bucket Fill", "Clear Canvas"]
-    }
-]).then(function (user) {
-
-    if (user.toolCommands === "Create Canvas") {
-        console.log("==============================================");
-        console.log("Creating Canvas...");
-        createCanvas();
-        console.log("Canvas Created!");
-        console.log("==============================================");
-    }
-    else if (user.toolCommands === "Draw Line 1") {
-        drawLine();
-    }
-    else if (user.toolCommands === "Draw Line 2") {
-        drawLine();
-    }
-    else if (user.toolCommands === "Draw Rectangle") {
-        drawRectangle();
-    }
-    else if (user.toolCommands === "Bucket Fill") {
-        bucketFill();
-    }
-    else {
-
-    }
-});
-
-
-var main = require('./bin/main.js');
+        if (command.toolCommands === "Create Canvas") {
+            console.log("==============================================");
+            console.log("Creating Canvas...");
+            screenBuffer = createCanvas(commandTools, screenBuffer);
+            console.log("Canvas Created!");
+            console.log("==============================================");
+            return screenBuffer;
+        }
+        else if (command.toolCommands === "Draw Line 1") {
+            screenBuffer = createLine(commandTools, screenBuffer);
+        }
+        else if (command.toolCommands === "Draw Line 2") {
+            screenBuffer = createLine(commandTools, screenBuffer);
+            return screenBuffer;
+        }
+        else if (command.toolCommands === "Draw Rectangle") {
+            screenBuffer = createRectangle(commandTools, screenBuffer);
+            return screenBuffer;
+        }
+        else if (command.toolCommands === "Bucket Fill") {
+            screenBuffer = bucketFill(commandTools, screenBuffer);
+            return screenBuffer;
+        }
+        else {
+            screenBuffer = clearCanvas(commandTools, screenBuffer);
+            return screenBuffer;
+        }
+    })
+};
 
 function printScreenBuffer(screenBuffer) {
     screenBuffer.map(function (state) {
@@ -62,7 +71,3 @@ function error(message) {
     console.log('Error', message);
     process.exit(1);
 }
-
-main(process.argv)
-    .then(printScreenBuffer)
-    .catch(error);
--- DrawingCLI/lib/canvas.js
+++ DrawingCLI/lib/canvas.js
@@ -1,26 +1,56 @@
-module.exports = function createCanvas(commandTools, inScreenBuffer) {
-    var outScreenBuffer = clone(inScreenBuffer);
-    if (inScreenBuffer.length !== 1) {
+module.exports = {
+
+    createCanvas: function createCanvas(commandTools, inScreenBuffer) {
+        var outScreenBuffer = clone(inScreenBuffer);
+        if (inScreenBuffer.length !== 1) {
+            return outScreenBuffer;
+        }
+
+        var width = parseInt(commandTools[1], 10);
+        var height = parseInt(commandTools[2], 10);
+        var boxHeight = height + 2;
+        var boxWidth = width + 2;
+
+        outScreenBuffer = new Array(boxHeight);
+
+        for (var j = 0; j < boxHeight; j++) {
+            outScreenBuffer[j] = new Array(boxWidth).fill(' ');
+            outScreenBuffer[j][0] = '|';
+            outScreenBuffer[j][boxWidth - 1] = '|';
+        }
+
+        for (var i = 0; i < boxWidth; i++) {
+            outScreenBuffer[0][i] = '-';
+            outScreenBuffer[boxHeight - 1][i] = '-';
+        }
+
+        return outScreenBuffer;
+    },
+
+    clearCanvas: function clearCanvas(commandTools, inScreenBuffer) {
+        var outScreenBuffer = clone(inScreenBuffer);
+        if (inScreenBuffer.length !== 1) {
+            return outScreenBuffer;
+        }
+
+        var width = parseInt(commandTools[1], 10);
+        var height = parseInt(commandTools[2], 10);
+        var boxHeight = height + 2;
+        var boxWidth = width + 2;
+
+        outScreenBuffer = new Array(boxHeight);
+
+        for (var j = 0; j < boxHeight; j++) {
+            outScreenBuffer[j] = new Array(boxWidth).fill(' ');
+            outScreenBuffer[j][0] = '|';
+            outScreenBuffer[j][boxWidth - 1] = '|';
+        }
+
+        for (var i = 0; i < boxWidth; i++) {
+            outScreenBuffer[0][i] = '-';
+            outScreenBuffer[boxHeight - 1][i] = '-';
+        }
+
         return outScreenBuffer;
     }
-
-    var width = parseInt(commandTools[1], 10);
-    var height = parseInt(commandTools[2], 10);
-    var boxHeight = height + 2;
-    var boxWidth = width + 2;
-
-    outScreenBuffer = new Array(boxHeight);
-
-    for (var j = 0; j < boxHeight; j++) {
-        outScreenBuffer[j] = new Array(boxWidth).fill(' ');
-        outScreenBuffer[j][0] = '|';
-        outScreenBuffer[j][boxWidth - 1] = '|';
-    }
-
-    for (var i = 0; i < boxWidth; i++) {
-        outScreenBuffer[0][i] = '-';
-        outScreenBuffer[boxHeight - 1][i] = '-';
-    }
-
-    return outScreenBuffer;
 };
